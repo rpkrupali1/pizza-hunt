8ffb58b3930e5557b213201e57dfdd8336618960
rpkrupali1 <rpkrupali@gmail.com>
2022-04-18 17:38:53 -0500

    restructure

--- models/Comment.js
+++ models/Comment.js
@@ -1,32 +1,32 @@
-const { Schema, model, Types } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const replySchema = new Schema(
+const ReplySchema = new Schema(
   {
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
+      default: () => new Types.ObjectId()
     },
     replyBody: {
       type: String,
-      required: true,
-      trim: true,
+      required: true
     },
     writtenBy: {
       type: String,
       required: true,
-      trim: true,
+      trim: true
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
   },
   {
     toJSON: {
-      getters: true,
-    },
+      getters: true
+    }
   }
 );
 
@@ -34,35 +34,33 @@ const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
-      required: true,
-      trim: true,
+      required: true
     },
     commentBody: {
       type: String,
-      required: true,
-      trim: true,
+      required: true
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
+      get: createdAtVal => dateFormat(createdAtVal)
     },
-    replies: [replySchema],
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
   {
     toJSON: {
       virtuals: true,
-      getters: true,
+      getters: true
     },
-    id: false,
+    id: false
   }
 );
 
-// get total count of replies on retrieval
-CommentSchema.virtual("replyCount").get(function () {
+CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
 });
 
-const Comment = model("Comment", CommentSchema);
+const Comment = model('Comment', CommentSchema);
 
 module.exports = Comment;
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,17 +1,21 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
   addComment,
   removeComment,
   addReply,
-  removeReply,
-} = require("../../controllers/comment-controller");
+  removeReply
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
-router.route("/:pizzaId").post(addComment);
+router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
 
-router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
